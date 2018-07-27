c891f41b499a8874c9a1c8f92a4b3e721a0a3bb8
Nikolaus Kreiling <nakreiling@gmail.com>
2018-07-27 18:39:57 -0400

    MorseCode assignment
    Finished MorseCode assignment

---
+++ MorseCode/MorseCode/Code.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Nikolaus Kreiling (nkreilin)
+ * ITCS 3112-080
+ * July 23, 2018
+ */ 
+
+namespace MorseCode
+{
+    class Code
+    {
+        // Stores the letters from Morse.txt
+        private static ArrayList morseKey;
+        // Stores the morse values from Morse.txt
+        private static ArrayList morseValue;
+
+        // Default constructor
+        public Code() { }
+
+        public Code(ArrayList keys, ArrayList values)
+        {
+            morseKey = keys;
+            morseValue = values;
+        }
+
+        /* 
+         * Requests a string to be input by the user
+         * @return input - the string entered by the user
+         */
+        public String TakeUserInput()
+        {
+            Console.WriteLine("Please enter a sentance (containing only letters and numbers) " +
+                "that you would like to convert to morse code.");
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+        /*
+         * Converts a string into morse code by iterating through the string and checking if values are in
+         * the ArrayList of letters from Morse.txt
+         * Prints the morse code output of the converted string at end of method
+         * Exits the method if a character that cannot be converted to morse code is entered
+         * @param userSentence - a sentence entered by the user to be converted to morse code
+         */ 
+        public void ConvertToMorseCode(string userSentence)
+        {
+            string morseCodeOutput = "";
+            foreach (char ch in userSentence)
+            {
+                if (Char.IsWhiteSpace(ch))
+                {
+                    morseCodeOutput += "   ";
+                }
+                else if (morseKey.Contains(ch.ToString().ToUpper()))
+                {
+                    int index = morseKey.IndexOf(ch.ToString().ToUpper());
+                    morseCodeOutput += morseValue[index] + " ";
+                }
+                else
+                {
+                    Console.WriteLine(ch + "  You entered a character that could not be converted to morse code...");
+                    return;
+                }
+            }
+            Console.WriteLine(morseCodeOutput);
+        }
+
+
+    }
+}
---
+++ MorseCode/MorseCode/Morse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+/*
+ * Nikolaus Kreiling (nkreilin)
+ * ITCS 3112-080
+ * July 23, 2018
+ */
+
+namespace MorseCode
+{
+    class Morse
+    {
+        static void Main(string[] args)
+        {
+            // Try-catch block for error handling
+            try
+            {
+                // User input for Morse.txt file path
+                Console.WriteLine("Please enter the path for the Morse Code file you would like to read from:");
+                string morseFilePath = Console.ReadLine();
+
+                // Reads all lines of Morse.txt into a string array
+                string[] morseFile = System.IO.File.ReadAllLines(morseFilePath);
+
+                // Stores letters from Morse.txt
+                ArrayList morseKey = new ArrayList();
+                // Stores morse values from Morse.txt
+                ArrayList morseValue = new ArrayList();
+
+                // Iterates through the string array Morse.txt was stored in
+                // Stores the letters into the ArrayList morseKey and the morse values into the ArrayList morseValue
+                foreach (string str in morseFile)
+                {
+                    string split = " ";
+                    string[] morseSplit = str.Split(split);
+                    morseKey.Add(morseSplit[0]);
+                    morseValue.Add(morseSplit[1]);
+                }
+
+                // Creates new Code object from Code.cs
+                Code codeObject = new Code(morseKey, morseValue);
+
+                string userSentence = "";
+
+                // Continues asking the user to input sentences to be converted to morse code until a sentinel value (0) is entered
+                while (!userSentence.Equals("0"))
+                {
+                    Console.WriteLine("This program will continually request for a string to be converted to morse code.");
+                    Console.WriteLine("Enter '0' to exit.");
+                    userSentence = codeObject.TakeUserInput();
+                    codeObject.ConvertToMorseCode(userSentence);
+                }
+            }
+            // Catches a FileNotFoundException
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("An error occured. The file was not found. See StackTrace below:");
+                Console.WriteLine(ex.ToString());
+            }
+            // Catches all errors that are not FileNotFoundException
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred. See StackTrace below:");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Goodbye!");
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
